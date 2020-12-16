a7d3fd3020c0ab9254bf3bfbb520ffaa1a55b54e
Postboll <ro1228@icloud.com>
2020-12-16 20:43:12 +0900

    残り

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workout_management/add/add_page.dart';
-
 import 'main_model.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
